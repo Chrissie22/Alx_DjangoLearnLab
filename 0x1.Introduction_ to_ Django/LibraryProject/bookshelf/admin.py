3dd079bc79bba95a62a44e31184be560bd75873e
Chrissie22 <christabel.ojobolo@gmail.com>
2024-08-20 15:11:02 +0100

    This project demonstrates how to use Django’s built-in admin interface to perform data management tasks efficiently.

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -3,6 +3,7 @@ from .models import Book
 
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
-    list_display = ("title", "author", "publication_year")
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ("title", "author")
     search_fields = ("title", "author", "publication_year")
 admin.site.register(Book)
