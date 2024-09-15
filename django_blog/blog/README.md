b7750b0b046be462f29fc7f6e066d747eba914bf
Chrissie22 <christabel.ojobolo@gmail.com>
2024-09-15 15:47:43 +0100

    Building a Complete Django Application

---
+++ django_blog/blog/README.md
@@ -0,0 +1,24 @@
+<!-- Documentaion -->
+
+<!-- Building a complete Django Appliction  -->
+
+<!-- Project Setup  -->
+
+Install Django:
+pip install django.
+
+Create Your Django Project:
+
+django-admin startproject django_blog.
+
+Navigate into your project directory (cd LibraryProject).
+Create a README.md file inside the djangoblog project.
+
+Start the development server using: 
+python manage.py runserver.
+
+<!-- Application setup -->
+
+Create Django Application:
+
+python manage.py startapp blog
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 from .models import Post
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -11,8 +14,19 @@ def posts(request):
     posts = Post.objects.all()
     return render(request, 'blog/posts.html', {'posts': posts})
 
-def login_view(request):
-    return render(request, 'blog/login.html')
+@login_required
+
+def profile(request):
+    return render(request, 'profile.html')
+
+
 
 def register(request):
-    return render(request, 'blog/register.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.Post)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+        return render(request, 'register.html', {'form': form})
