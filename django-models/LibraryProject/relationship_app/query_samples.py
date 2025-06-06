ec403585d74d240e1683a398d15f17757b2881ae
Chrissie22 <christabel.ojobolo@gmail.com>
2025-06-06 19:37:11 +0100

    updated query_samples.py to contain: [Author.objects.get(name=author_name)]

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -9,8 +9,8 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 author_name = "Chrissel Techworld"
 author = Author.objects.get(name=author_name)
-books_by_author = Book.objects.set.all()
-
+books_by_author = Book.objects.filter(author=author)
+print(f"Books by {author_name}: {[book.title for book in books_by_author]}")
 
 library_name = "Central Library"
 library = Library.objects.get(name=library_name)
