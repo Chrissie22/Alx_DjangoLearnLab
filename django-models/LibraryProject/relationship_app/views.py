08a8bb6349f3727f32d941116b839f4102875f56
Chrissie22 <christabel.ojobolo@gmail.com>
2025-06-07 14:14:56 +0100

    Utilize Django’s ListView or DetailView to structure this class-based view.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -20,5 +20,12 @@ def list_books(request):
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
-    context_object_name = "library"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        library = self.get_object()
+        context['books'] = library.books.all()
+        return context
+            
+        
     
