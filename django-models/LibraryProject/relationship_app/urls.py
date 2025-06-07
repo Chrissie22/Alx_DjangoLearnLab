46b1412a4fa912aa7ff28a1c74b8a9c674de6e17
Chrissie22 <christabel.ojobolo@gmail.com>
2025-06-07 14:39:25 +0100

    make an update on the url path

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,5 +5,5 @@ from .views import list_books, LibraryDetailView
 app_name = "relationship_app"
 urlpatterns = [
     path("books/", list_books, name="list_books"),
-    path("relationship/library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
 ]
