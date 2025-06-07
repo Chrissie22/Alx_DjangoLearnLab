3c3a840f00f430c4c5821751924904aaf82802e1
Chrissie22 <christabel.ojobolo@gmail.com>
2025-06-07 15:11:01 +0100

    feat(views): add home view function and enhance library detail view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -12,7 +12,7 @@ from django.views.generic import DetailView
 # Create your views here.
 
 def list_books(request): 
-    books = Book.objects.all()
+    books = Book.objects.select_related('author').all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
  
@@ -21,6 +21,15 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        library = self.get_object()
+        context['books'] = self.objects.books.select_related('author').all()
+        return context
+    
+def home(request):
+    return render(request, 'relationship_app/home.html')
             
         
     
