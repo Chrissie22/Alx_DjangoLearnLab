f2c6bf147eba84f4fe0611d27a35ed8ea0084f68
Chrissie22 <christabel.ojobolo@gmail.com>
2025-06-21 00:23:37 +0100

    Add user authentication views, templates, and URLs

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('', home, name='home'),
     path("books/", list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("register/", views.register, name="register"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Author, Book, Librarian
 from .models import Library
 from django.views.generic.detail import DetailView
 from django.views import generic
 from django.views.generic import ListView, CreateView
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
 
 
 
@@ -25,9 +28,44 @@ class LibraryDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         library = self.get_object()
-        context['books'] = self.objects.books.select_related('author').all()
+        context['books'] = library.books.select_related('author').all()
         return context
     
+ 
+ 
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    else:
+        form = AuthenticationForm()
+    return render(request, "relationship_app/login.html", {"form": form})
+
+def user_logout(request):
+    logout(request)
+    return render(request, "relationship_app/logout.html")
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.saved()
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Registeration failed. Please try again.")
+            
+               
 def home(request):
     return render(request, 'relationship_app/home.html')
             
