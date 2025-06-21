855f089fe4011ba74884f53c6e1a9947a1e5683b
Chrissie22 <christabel.ojobolo@gmail.com>
2025-06-21 17:38:54 +0100

    Utilize Django’s built-in views for handling user authentication

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,7 @@ from .models import Library
 from django.views.generic.detail import DetailView
 from django.views import generic
 from django.views.generic import ListView, CreateView
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
 
